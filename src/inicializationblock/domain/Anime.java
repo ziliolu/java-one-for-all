e407e53bfa5ba347bae1aa80d5cd653ea7b5d354
Luiza Picoli <luzilio.p@gmail.com>
2022-06-16 11:09:02 -0300

    initialization block

---
+++ src/inicializationblock/domain/Anime.java
@@ -0,0 +1,39 @@
+package inicializationblock.domain;
+
+public class Anime {
+
+    private String name;
+    private int[] episodes;
+
+    // 1 - Memory is allocated for the object
+    // 2 - Each class attribute is initialized
+    // 3 - Initialization block is executed
+    // 4 - Constructed is executed
+
+
+    {
+        System.out.println("Inside init block");
+    }
+
+    public Anime(String name) {
+        this.name = name;
+    }
+
+    public Anime() {
+        episodes = new int[100];
+        for (int i = 0; i < episodes.length; i++) {
+            episodes[i] = i + 1;
+        }
+        for (int episode : episodes) {
+            System.out.println(episode + " ");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int[] getEpisodes() {
+        return episodes;
+    }
+}
---
+++ src/inicializationblock/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package inicializationblock.test;
+
+import inicializationblock.domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+        Anime anime2 = new Anime();
+    }
+}
