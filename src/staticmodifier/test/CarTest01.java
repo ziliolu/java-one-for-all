9a4e334078c1caec9b7255ac920190eb73246897
Luiza Picoli <luzilio.p@gmail.com>
2022-06-16 11:37:35 -0300

    static modifier class

--- src/staticmodifier/test/CarTest01.java
+++ src/staticmodifier/test/CarTest01.java
@@ -8,11 +8,9 @@ public class CarTest01 {
         Car car2 = new Car("Mercedes", 285D);
         Car car3 = new Car("Audi", 290D);
 
-
         car1.print();
         car2.print();
         car3.print();
-
 
         car1.setSpeedLimit(300);
         car2.print();
