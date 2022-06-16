6fe5f05e8c8fbe4597e80d5ecc808281a73133dc
Luiza Picoli <luzilio.p@gmail.com>
2022-06-16 11:36:46 -0300

    static modifier class

---
+++ src/staticmodifier/domain/Car.java
@@ -0,0 +1,47 @@
+package staticmodifier.domain;
+
+public class Car {
+    private String name;
+    private double maxSpeed;
+    public static double speedLimit = 250;
+
+    /*      static -> means it belongs to the class itself
+            if I change the value of a static attribute all the
+            objects of this class will be changed as well         */
+
+    public Car(String name, double maxSpeed) {
+        this.name = name;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public void print(){
+        System.out.println("-------------");
+        System.out.println("Name: "+ this.name);
+        System.out.println("Max speed: "+ this.maxSpeed);
+        System.out.println("Speed limit: "+ this.speedLimit);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(double maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public double getSpeedLimit() {
+        return speedLimit;
+    }
+
+    public void setSpeedLimit(double speedLimit) {
+        this.speedLimit = speedLimit;
+    }
+}
---
+++ src/staticmodifier/test/CarTest01.java
@@ -0,0 +1,20 @@
+package staticmodifier.test;
+
+import staticmodifier.domain.Car;
+
+public class CarTest01 {
+    public static void main(String[] args) {
+        Car car1 = new Car("BMW", 270D);
+        Car car2 = new Car("Mercedes", 285D);
+        Car car3 = new Car("Audi", 290D);
+
+
+        car1.print();
+        car2.print();
+        car3.print();
+
+
+        car1.setSpeedLimit(300);
+        car2.print();
+    }
+}
